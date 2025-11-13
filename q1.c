7a5b528be433d67ad44060c7f182ee08e63406f1
anasfurqan250577 <k250577@nu.edu.pk>
2025-11-13 20:02:12 +0500

    Create q1.c

---
+++ q1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum=0;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+        arr[i] = sum;
+    }
+    printf("\n");
+    for(int i=0; i<n; i++){
+        printf("%d ", arr[i]);
+    }
+    
+    return 0;
+}
